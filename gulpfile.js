27bbfceef5e61482069a35d34dadb4b0c95e86d1
Raymond <raymondurrutia2@yahoo.com>
2017-09-11 21:38:13 -0400

    Updated gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,49 @@
 var gulp = require('gulp');
 
 // Included Plugins
-var sass = require('gulp-sass');
-var prefix = require('gulp-autoprefixer');
+var sass = require('gulp-sass'),
+	prefix = require('gulp-autoprefixer'),
+	concat = require('gulp-concat'),
+	rename = require('gulp-rename'),
+	uglify = require('gulp-uglify'),
+	connect = require('gulp-connect');
 
 // Compile sass
-gulp.task('sass', function(){
-  return gulp.src('dev/scss/main.scss')
-    .pipe(sass())
-    .pipe(prefix('last 2 versions'))
-    .pipe(gulp.dest('assets/css'));
+gulp.task('sass', function() {
+  	return gulp.src('dev/scss/main.scss')
+	    .pipe(sass())
+	    .pipe(prefix('last 2 versions'))
+	    .pipe(gulp.dest('assets/css'))
+	    .pipe(connect.reload());
 });
 
-gulp.task('watch', function() {
+// Combine and minify js
+gulp.task('js', function() {
+    return gulp.src('dev/js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('assets/js'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('assets/js'))
+        .pipe(connect.reload());
+});
 
+// Live reload
+gulp.task('connect', function() {
+	connect.server({
+		root:'.',
+		livereload: true
+	})
+});
+
+// Watch for changes
+gulp.task('watch', function() {
+	// Watch for js changes
+	gulp.watch('dev/js/*.js', ['js']);
     // Watch for scss changes
     gulp.watch('dev/scss/main.scss', ['sass']);
 
 });
 
 // Default Task
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['sass', 'js', 'connect', 'watch']);
