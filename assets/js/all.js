465cebf6d85e6fbab6671fbe3a8378b3064ade29
Raymond <raymondurrutia2@yahoo.com>
2017-09-29 20:38:04 -0400

    Added jquery to hide/show navbar on scroll

--- assets/js/all.js
+++ assets/js/all.js
@@ -39,7 +39,7 @@ INTRO SCROLL TO
 		$("#introScrollButton").click(function(e) {
 			// Prevent browser from jumping to the anchor when clicked 
 			e.preventDefault();
-			$('html, body').animate({ scrollTop: $("#intro-greeting").offset().top - 50}, 1000);
+			$('html, body').animate({ scrollTop: $("#intro-greeting").offset().top }, 1000);
 		});
 	});
 
@@ -99,6 +99,40 @@ ICON BAR ROTATE
 
 
 
+(function($) {
+    var didScroll;
+    var $navbar = $('nav');
+    var $scrollPosition = $(window).scrollTop();
+    var $landingImgHt = $('.landing').innerHeight();
+
+    // On scroll, set did scroll to true
+    $(window).scroll(function() {
+          didScroll = true;
+    });
+
+    // If didScroll is true run hasScrolled fn and reset didScroll status
+    setInterval(function() {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+          }
+    }, 150);
+
+    function hasScrolled() {
+        // Get current scroll position
+        var currentScrollPosition = $(window).scrollTop();
+
+            // If the current scroll position is more than the previous scroll position hide the navbar
+            if (currentScrollPosition > $landingImgHt && currentScrollPosition > $scrollPosition) {
+                $navbar.slideUp('fast');
+            } else {
+                $navbar.slideDown('fast');
+            }
+
+            // Set the previous scroll position to this current scroll position
+            $scrollPosition = currentScrollPosition;
+    }
+})(jQuery);
 // When a button is clicked get the category of that button
 $('.portfolio-category-btn').click(function() {
     $('.portfolio-category-btn').removeClass('category-btn-active');
